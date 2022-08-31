eee89aec1801eea8eaff6f847149759065621425
Ada0910 <1355948107@qq.com>
2022-08-31 22:27:02 +0800

    新建软件设计七大原则

---
+++ software-design-demo/src/test/java/com/ada/software/design/SoftwareDesignDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.software.design;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareDesignDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
