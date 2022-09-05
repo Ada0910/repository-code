6943a021f9b2ffa4217a9405b25dfa77491f0e5a
Ada0910 <1355948107@qq.com>
2022-09-05 23:33:17 +0800

    初始化多线程代码

---
+++ multi-thread-demo/src/test/java/com/ada/multi/thread/MultiThreadDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.multi.thread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiThreadDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
