7ee091d6ee7dacaf8824e65a35530679ab0f6e58
Ada0910 <1355948107@qq.com>
2022-09-08 23:11:53 +0800

    JAVA中如何使用多线程

---
+++ multi-thread-demo/src/main/java/com/ada/multi/thread/start/MyThread.java
@@ -0,0 +1,24 @@
+package com.ada.multi.thread.start;
+
+/**
+ * <b><code></code></b>
+ * <p/>
+ * JAVA实现多线程的方法一：继承Thread类,本质也是一个Runnable接口
+ * <p/>
+ *
+ * @Date: 2022/9/8 22:40
+ * @Author xwn
+ * @Version 1.0.0.1
+ */
+public class MyThread extends Thread {
+
+    @Override
+    public void run() {
+        System.out.println("JAVA实现多线程的方法一：继承Thread类");
+    }
+
+    public static void main(String[] args) {
+        MyThread myThread = new MyThread();
+        myThread.start();
+    }
+}
