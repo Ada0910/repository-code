89f151775ca4af75edfd4cbd0e838b6512ca4060
xieweining <xieweining@chinaums.com>
2022-09-27 15:50:30 +0800

    顶层接口

---
+++ software-design-demo/src/main/java/com/ada/software/design/proxy/Person.java
@@ -0,0 +1,19 @@
+package com.ada.software.design.proxy;
+
+/**
+ *
+ * <b><code></code></b>
+ * <p/>
+ * 顶层接口类
+ * <p/>
+ * <b>Creation Time:</b> 2022/9/27 11:12
+ * @author xiewn
+ * @version 1.0.0.1
+ *
+ * @since gnete 1.0.0.1
+ */
+public interface Person {
+
+	//找对象
+	public void findLove();
+}
