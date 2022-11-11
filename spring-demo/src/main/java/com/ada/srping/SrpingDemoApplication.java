b4445671377c9e5702818a04610242482a4d0034
xieweining <xieweining@chinaums.com>
2022-11-11 16:49:37 +0800

    Spring 源码入门一

---
+++ spring-demo/src/main/java/com/ada/srping/SrpingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ada.srping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingDemoApplication.class, args);
+	}
+
+}
---
+++ spring-demo/src/test/java/com/ada/srping/SrpingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.srping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
