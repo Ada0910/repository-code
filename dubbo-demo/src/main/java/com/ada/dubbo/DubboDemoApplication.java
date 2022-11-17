d492418f7b133bc5de9730d63a05c93df5322cbe
Ada0910 <1355948107@qq.com>
2022-11-17 23:13:01 +0800

    新增dubbo模块

---
+++ dubbo-demo/src/main/java/com/ada/dubbo/DubboDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ada.dubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubboDemoApplication.class, args);
+	}
+
+}
---
+++ dubbo-demo/src/test/java/com/ada/dubbo/DubboDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.dubbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
