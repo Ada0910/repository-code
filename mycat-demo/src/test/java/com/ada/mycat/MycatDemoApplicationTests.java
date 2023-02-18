886db52cb85925ced1656c49d848857c017660dc
Ada0910 <1355948107@qq.com>
2023-02-18 21:36:09 +0800

    Mycat基础

---
+++ mycat-demo/src/test/java/com/ada/mycat/MycatDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.mycat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycatDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
