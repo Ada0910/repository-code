46d7e4680ab3dabbe3cd4bb554653d5411685177
Ada0910 <1355948107@qq.com>
2023-03-01 00:01:59 +0800

    Dubbo自定义扩展点

--- dubbo-demo/spi-demo/src/main/java/com/ada/spi/dubbo/MyProtocolTest.java
+++ dubbo-demo/spi-demo/src/main/java/com/ada/spi/dubbo/MyProtocolTest.java
@@ -1,7 +1,11 @@
 package com.ada.spi.dubbo;
 
+import org.apache.dubbo.common.URL;
 import org.apache.dubbo.common.extension.ExtensionLoader;
+import org.apache.dubbo.rpc.Filter;
 import org.apache.dubbo.rpc.Protocol;
+
+import java.util.List;
 
 /**
  *
@@ -26,7 +30,11 @@ public class MyProtocolTest {
 		Protocol protocol = ExtensionLoader.getExtensionLoader(Protocol.class).getExtension("myprotocol");
 		System.out.println(protocol.getDefaultPort());
 
-		Compiler protocol2=ExtensionLoader.getExtensionLoader(Compiler.class).getAdaptiveExtension();
+		Compiler protocol2 = ExtensionLoader.getExtensionLoader(Compiler.class).getAdaptiveExtension();
 
+		URL url = new URL("", "", 0);
+		url = url.addParameter("cache", "cache");
+		List<Filter> list = ExtensionLoader.getExtensionLoader(Filter.class).getActivateExtension(url, "cache");
+		System.out.println(list.size());
 	}
 }
