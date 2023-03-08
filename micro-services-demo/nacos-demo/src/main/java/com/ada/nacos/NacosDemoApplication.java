0bf89e846534a7681b724eeef19d16b4f7d58258
Ada0910 <1355948107@qq.com>
2023-03-09 00:02:20 +0800

    新增nacos

---
+++ micro-services-demo/nacos-demo/src/main/java/com/ada/nacos/NacosDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ada.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NacosDemoApplication.class, args);
+	}
+
+}
