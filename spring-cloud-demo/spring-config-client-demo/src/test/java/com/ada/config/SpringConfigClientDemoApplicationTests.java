f464c4e8d5e5b5493dd7fb9f2484bebc51a93e57
Ada0910 <1355948107@qq.com>
2023-03-28 23:56:03 +0800

    新增client-config

---
+++ spring-cloud-demo/spring-config-client-demo/src/test/java/com/ada/config/SpringConfigClientDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigClientDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
