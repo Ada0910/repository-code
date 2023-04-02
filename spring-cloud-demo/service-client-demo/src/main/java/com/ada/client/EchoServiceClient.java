97439111f72a83b59ff9cf3c69e4d79a38b2745e
Ada0910 <1355948107@qq.com>
2023-04-02 20:58:26 +0800

    新增注释

--- spring-cloud-demo/service-client-demo/src/main/java/com/ada/client/EchoServiceClient.java
+++ spring-cloud-demo/service-client-demo/src/main/java/com/ada/client/EchoServiceClient.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.PathVariable;
  * <b><code></code></b>
  * <p/>
  *  客户端打印类
+ *
+ *   @FeignClient("config-client")  说明调用名称叫config-client的服务
+ *
  * <p/>
  *
  * @Date: 2023/03/30 23:55
@@ -18,7 +21,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient("config-client")
 public interface EchoServiceClient {
     
-    
+    /**
+     *  使用该方法就相当于是要调用服务名称叫config-client，然后/services/name的controller调用方法
+     */
     @GetMapping(value = "/services/name")
     String services();
 }
