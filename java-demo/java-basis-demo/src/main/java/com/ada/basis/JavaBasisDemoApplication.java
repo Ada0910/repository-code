9c929bdc75ea29bbdba029d1a6c49c490f2740ad
xieweining <xieweining@chinaums.com>
2023-04-04 10:48:04 +0800

    初始化Java 样例

---
+++ java-demo/java-basis-demo/src/main/java/com/ada/basis/JavaBasisDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ada.basis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBasisDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBasisDemoApplication.class, args);
+	}
+
+}
---
+++ java-demo/java-basis-demo/src/test/java/com/ada/basis/JavaBasisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.basis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBasisDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
