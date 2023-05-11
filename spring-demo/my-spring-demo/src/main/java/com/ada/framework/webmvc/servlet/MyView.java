b09623f5bab367698c9ec2263eb51e2e8085858c
Ada0910 <1355948107@qq.com>
2023-05-11 21:57:08 +0800

    手写Spring2.0之SpringMVC

--- spring-demo/my-spring-demo/src/main/java/com/ada/framework/webmvc/servlet/MyView.java
+++ spring-demo/my-spring-demo/src/main/java/com/ada/framework/webmvc/servlet/MyView.java
@@ -3,7 +3,10 @@ package com.ada.framework.webmvc.servlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.RandomAccessFile;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -19,6 +22,8 @@ import java.util.Map;
  */
 public class MyView {
 
+	public final String DEFAULT_CONTENT_TYPE = "text/html;charset= utf-8";
+
 	private File viewFile;
 
 	public MyView(File viewFile) {
@@ -26,6 +31,40 @@ public class MyView {
 	}
 
 	public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		StringBuffer sb = new StringBuffer();
+		RandomAccessFile ra = new RandomAccessFile(this.viewFile, "r");
+		String line = null;
+		while (null != (line = ra.readLine())) {
+			line = new String(line.getBytes("ISO-8859-1"), "utf-8");
+			Pattern pattern = Pattern.compile("￥\\{[^\\}]+\\}", Pattern.CASE_INSENSITIVE);
+			Matcher matcher = pattern.matcher(line);
+			while (matcher.find()) {
+				String paramName = matcher.group();
+				paramName = paramName.replaceAll("￥\\{|\\}", "");
+				Object paramValue = model.get(paramName);
+				if (null == paramValue) {
+					continue;
+				}
+				line = matcher.replaceFirst(makeStringForRegExp(paramValue.toString()));
+				matcher = pattern.matcher(line);
+			}
+			sb.append(line);
+		}
 
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType(DEFAULT_CONTENT_TYPE);
+		response.getWriter().write(sb.toString());
+	}
+
+	//处理特殊字符
+	public static String makeStringForRegExp(String str) {
+		return str.replace("\\", "\\\\").replace("*", "\\*")
+				.replace("+", "\\+").replace("|", "\\|")
+				.replace("{", "\\{").replace("}", "\\}")
+				.replace("(", "\\(").replace(")", "\\)")
+				.replace("^", "\\^").replace("$", "\\$")
+				.replace("[", "\\[").replace("]", "\\]")
+				.replace("?", "\\?").replace(",", "\\,")
+				.replace(".", "\\.").replace("&", "\\&");
 	}
 }
