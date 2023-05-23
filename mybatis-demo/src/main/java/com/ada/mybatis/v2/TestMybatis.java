e7f9281c98d3660623997e9e209e866931eda52e
xieweining <xieweining@chinaums.com>
2023-05-23 14:25:25 +0800

    手写mybatis优化-v1、2版本优化

--- mybatis-demo/src/main/java/com/ada/mybatis/v2/TestMybatis.java
+++ mybatis-demo/src/main/java/com/ada/mybatis/v2/TestMybatis.java
@@ -1,8 +1,8 @@
 package com.ada.mybatis.v2;
 
 
-import com.ada.mybatis.v1.mapper.Blog;
-import com.ada.mybatis.v1.mapper.BlogMapper;
+import com.ada.mybatis.v2.mapper.Blog;
+import com.ada.mybatis.v2.mapper.BlogMapper;
 import com.ada.mybatis.v2.session.DefaultSqlSession;
 import com.ada.mybatis.v2.session.SqlSessionFactory;
 
@@ -13,7 +13,7 @@ public class TestMybatis {
         DefaultSqlSession sqlSession = factory.build().openSqlSession();
         // 获取MapperProxy代理
         BlogMapper mapper = sqlSession.getMapper(BlogMapper.class);
-        Blog blog = mapper.selectBlogById(1);
+        Blog blog = mapper.selectBlogById(2);
 
         System.out.println("第一次查询: " + blog);
         System.out.println();
