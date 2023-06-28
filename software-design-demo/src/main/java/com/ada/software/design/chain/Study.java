8f4c666d2774f804632f2bb8b922796bc9f0b07b
Ada0910 <1355948107@qq.com>
2023-06-28 20:03:05 +0800

    设计模式之责任链模式

---
+++ software-design-demo/src/main/java/com/ada/software/design/chain/Study.java
@@ -0,0 +1,19 @@
+package com.ada.software.design.chain;
+
+/**
+ *
+ * <b><code></code></b>
+ * <p/>  
+ *
+ * <p/>
+ *
+ * @Date: 2023/6/28 19:44
+ * @Author xwn
+ * @Version 1.0.0.1
+ *
+ */
+public class Study {
+	public void study() {
+		System.out.println("学习");
+	}
+}
