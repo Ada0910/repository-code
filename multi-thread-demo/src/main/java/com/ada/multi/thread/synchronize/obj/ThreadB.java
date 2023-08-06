075dbcf5a00b801d803f8f44ad823e7a210db330
Ada0910 <1355948107@qq.com>
2023-08-06 23:23:55 +0800

    synchronized锁的是对象

---
+++ multi-thread-demo/src/main/java/com/ada/multi/thread/synchronize/obj/ThreadB.java
@@ -0,0 +1,26 @@
+package com.ada.multi.thread.synchronize.obj;
+
+/**
+ *
+ * <b><code></code></b>
+ * <p/>  
+ *
+ * <p/>
+ *
+ * @Date: 2023/8/6 18:38
+ * @Author xwn
+ * @Version 1.0.0.1
+ *
+ */
+public class ThreadB extends Thread {
+	private MyObject object;
+
+	public ThreadB(MyObject object) {
+		this.object = object;
+	}
+
+	@Override
+	public void run() {
+		object.methodA();
+	}
+}
