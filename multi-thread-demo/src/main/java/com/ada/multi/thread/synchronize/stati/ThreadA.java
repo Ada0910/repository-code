551913aa2b333536b308faa32a243268b826f0f8
Ada0910 <1355948107@qq.com>
2023-08-13 16:17:18 +0800

    Class锁示例

---
+++ multi-thread-demo/src/main/java/com/ada/multi/thread/synchronize/stati/ThreadA.java
@@ -0,0 +1,21 @@
+package com.ada.multi.thread.synchronize.stati;
+
+/**
+ *
+ * <b><code></code></b>
+ * <p/>  
+ *
+ * <p/>
+ *
+ * @Date: 2023/8/13 15:54
+ * @Author xwn
+ * @Version 1.0.0.1
+ *
+ */
+public class ThreadA  extends  Thread{
+
+	@Override
+	public void run() {
+		Service.printA();
+	}
+}
