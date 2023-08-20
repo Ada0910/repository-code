83db68d3aa5c66a8bf4137eb2a005a4a6f75de56
Ada0910 <1355948107@qq.com>
2023-08-20 17:25:43 +0800

    交替打印demo

---
+++ multi-thread-demo/src/main/java/com/ada/multi/thread/pip/backup/BackupA.java
@@ -0,0 +1,26 @@
+package com.ada.multi.thread.pip.backup;
+
+/**
+ *
+ * <b><code></code></b>
+ * <p/>  
+ *
+ * <p/>
+ *
+ * @Date: 2023/8/20 17:17
+ * @Author xwn
+ * @Version 1.0.0.1
+ *
+ */
+public class BackupA extends Thread {
+	private DBTools tools;
+
+	public BackupA(DBTools tools) {
+		this.tools = tools;
+	}
+
+	@Override
+	public void run() {
+		tools.backupA();
+	}
+}
