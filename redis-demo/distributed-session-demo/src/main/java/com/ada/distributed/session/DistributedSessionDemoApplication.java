08dc2b66b382670f358c7c5f5da43c8a06b83e41
Ada0910 <1355948107@qq.com>
2024-04-23 00:39:32 +0800

    新增Spring-session分布式会话demo

---
+++ redis-demo/distributed-session-demo/src/main/java/com/ada/distributed/session/DistributedSessionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ada.distributed.session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedSessionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedSessionDemoApplication.class, args);
+	}
+
+}
---
+++ redis-demo/distributed-session-demo2/src/main/java/com/ada/distributed/session/controller/TestSessionController.java
@@ -0,0 +1,39 @@
+package com.ada.distributed.session.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * <b><code></code></b>
+ * <p/>  
+ * 分布式session 不一致的解决方案
+ * <p/>
+ *
+ * @Date: 2024/4/22 23:48
+ * @Author xwn
+ * @Version 1.0.0.1
+ *
+ */
+@Controller
+public class TestSessionController {
+
+	@Value("${server.port}")
+	private Integer projectPort;// 项目端口
+
+	@RequestMapping("/createSession")
+	public String createSession(HttpSession session, String name) {
+		session.setAttribute("name", name);
+		System.out.println("当前项目端口：" + projectPort + " 当前sessionId :" + session.getId() + "在Session中存入成功！");
+		return "当前项目端口：" + projectPort + " 当前sessionId :" + session.getId() + "在Session中存入成功！";
+	}
+
+	@RequestMapping("/getSession")
+	public String getSession(HttpSession session) {
+		System.out.println("当前项目端口：" + projectPort + " 当前sessionId :" + session.getId() + "  获取的姓名:" + session.getAttribute("name"));
+		return "当前项目端口：" + projectPort + " 当前sessionId :" + session.getId() + "  获取的姓名:" + session.getAttribute("name");
+	}
+}
